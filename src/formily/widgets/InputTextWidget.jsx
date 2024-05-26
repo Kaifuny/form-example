c585d408f9e408c24648fd5fc4125e8fcba7dbcd
Kaifuny <superbiger.github@gmail.com>
2024-05-26 12:59:11 +0800

    单向 binding 示例

--- src/formily/widgets/InputTextWidget.jsx
+++ src/formily/widgets/InputTextWidget.jsx
@@ -4,6 +4,9 @@ import { InputText } from '../../components/InputText';
 export const InputTextWidget = React.forwardRef((props, ref) => {
   const [value, setValue] = React.useState(props.value);
 
+  /**
+   * 这里 ref 暴露的方法，是将这个组件映射为一个 对象（json 结构）的关键
+   */
   React.useImperativeHandle(ref, () => ({
     value,
     onChange: (value) => {
