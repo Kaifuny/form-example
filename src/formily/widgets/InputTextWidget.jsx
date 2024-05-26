a165395fabefc816d52323b321ce656ff7429659
Kaifuny <superbiger.github@gmail.com>
2024-05-26 13:03:07 +0800

    单向 binding 示例

--- src/formily/widgets/InputTextWidget.jsx
+++ src/formily/widgets/InputTextWidget.jsx
@@ -7,6 +7,7 @@ export const InputTextWidget = React.forwardRef((props, ref) => {
   /**
    * 这里 ref 暴露的方法，是将这个组件映射为一个 对象（json 结构）的关键
    * 也就是注册 Widget 的时候需要定义一个 接口，这个接口就是 ref 暴露的方法
+   * 暂时推想的是，这里代码应该就是固定的，可能面向对象继承的方式，可以进一步简化对共有行为进行封装
    */
   React.useImperativeHandle(ref, () => ({
     value,
