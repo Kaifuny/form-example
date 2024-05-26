8b5c9f3226c5bf19aac38a74619cd03d7a317e77
Kaifuny <superbiger.github@gmail.com>
2024-05-26 12:54:45 +0800

    单向 binding 示例

--- src/formily/hooks/useForm.js
+++ src/formily/hooks/useForm.js
@@ -3,11 +3,25 @@ import React from "react";
 function useForm(fromDataParmas, toDataParmas) {
   const register_fields = [];
 
+  /* 
+  * InputText --> InputTextWidget 中完成注册 --> FormField 进行解包 --> 最后托管到 当前响应中
+  */
   React.useEffect(() => {
     register_fields.forEach(({ref, fromName, toName}) => {
       if (ref.current === null) return;
+      /**
+       * 这里可以输出 ref 看一下
+       * 无论是 form_field_ref 有一个 context 属性
+       */
       const form_field_ref = ref.current;
+      console.log(form_field_ref);
+
+      /**
+       * 这里通过 托管的 field_ref 来获取其下 widget_ref
+       */
       const widget_ref = form_field_ref.ref;
+      
+      // 这里可以根据 widget_ref 传递进来的 onChange 来进行数据更新
       widget_ref.current.onChange(fromDataParmas[fromName]);
 
       // 这里可以根据 register 传递进来的 formatter 来进行数据格式化
